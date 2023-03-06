f3ab11d6a4463ab39452144023192b269850ac6a
Vladimir Smiryagin <smitt.r77@yandex.ru>
2023-03-06 16:53:33 +0300

    add random 2d double array

--- Template/Program.cs
+++ Template/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Шаблоны 
 
 // Массив
-/*
+
 int[] CreateArray(int size)
 {
 	int[] array = new int[size];
@@ -13,10 +13,10 @@ int[] CreateArray(int size)
 	}
 	return array;
 }
-*/
+
 
 // Рандомный одномерный массив + вывод
-/*
+
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
 	int[] array = new int[size];
@@ -46,10 +46,9 @@ int max = Convert.ToInt32(Console.ReadLine());
 
 int[] newArray = CreateRandomArray(size, min, max);
 ShowArray(newArray);
-*/
 
 // Рандомный одномерный вещественный массив + вывод
-/*
+
 double[] CreateRandomDoubleArray(int size, int minValue, int maxValue)
 {
 	double[] array = new double[size];
@@ -77,10 +76,10 @@ int max = Convert.ToInt32(Console.ReadLine());
 
 double[] newArray = CreateRandomDoubleArray(size, min, max);
 ShowArray(newArray);
-*/
 
-// Рандомный двухмерный массив
-/*
+
+// Рандомный двухмерный массив + ввод
+
 int[,] CreateRandom2dArray()
 {
 	Console.Write("Input a number of rows: ");
@@ -112,4 +111,38 @@ void Show2dArray(int[,] array)
 
 int[,] newArray = CreateRandom2dArray();
 Show2dArray(newArray);
-*/
+
+
+// Рандомный двумерный массив вещественных чисел + ввод
+
+double[,] CreateRandom2dDoubleArray()
+{
+	Console.Write("Input a number of rows: ");
+	int rows = Convert.ToInt32(Console.ReadLine());
+	Console.Write("Input a number of columns: ");
+	int columns = Convert.ToInt32(Console.ReadLine());
+	Console.Write("Input a min possible value: ");
+	int minValue = Convert.ToInt32(Console.ReadLine());
+	Console.Write("Input a max possible value: ");
+	int maxValue = Convert.ToInt32(Console.ReadLine());
+
+	double[,] array = new double[rows, columns];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			array[i, j] = Math.Round((new Random().Next(minValue, maxValue + 1) + new Random().NextDouble()), 2);
+	return array;
+}
+
+void Show2dArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write(array[i, j] + " ");
+		Console.WriteLine();
+	}
+	Console.WriteLine();
+}
+
+double[,] newArray = CreateRandom2dDoubleArray();
+Show2dArray(newArray);
